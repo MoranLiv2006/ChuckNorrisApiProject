edb38286d16daf20d4332504747d1a6fe3c453e5
Moran Liv <moran.liv2006@gmail.com>
2024-05-14 18:01:28 -0700

    Added allure-report support

--- src/test/java/tests.java
+++ src/test/java/tests.java
@@ -1,15 +1,19 @@
+import io.qameta.allure.Step;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class tests extends BaseClass {
 
-    @Test(invocationCount = 5)
+    @Test(invocationCount = 5, description = "sdsds")
     public void randomJoke() {
         getRandomJoke(true);
         getRandomJoke(false);
+        Assert.assertEquals(1, 1, "yes!");
     }
 
     @Test(invocationCount = 5)
     public void categorisedJoke() {
         getRandomCategorisedJoke();
+        Assert.fail();
     }
 }
